f25c7277245c4bc2aa202ea6c23ae6b83643bc7e
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-09-05 23:50:49 -0700

    Validado y documentado

--- CalculadoraAhorro/src/Calculadora.java
+++ CalculadoraAhorro/src/Calculadora.java
@@ -3,29 +3,50 @@ import java.util.ArrayList;
 
 public class Calculadora {
 
+    /**
+     * Método que muestra la tabla de los cálculos.
+     *
+     * @param aportes lista de aportes mensuales.
+     * @param porcentajeInteres porcentaje de la tasa de interés.
+     */
     public void mostrarTabla(ArrayList<Float> aportes, Float porcentajeInteres) {
         System.out.printf("%-10s %-15s %-15s %-15s\n", "Mes", "Aporte", "Interés", "Total");
 
         int mes = 1;
-        Float totalNeto =  0f;
+        Float totalNeto = 0f;
         for (Float aporte : aportes) {
 
             Float interesCalculado = calcularCostoInteres(aporte, porcentajeInteres);
             Float totalCalculado = calcularTotalMes(aporte, interesCalculado);
 
             System.out.printf("%-10d $%-15.2f $%-15.2f $%-15.2f\n", mes, aporte, interesCalculado, totalCalculado);
-            
+
             mes++;
             totalNeto += totalCalculado;
-             
+
         }
-        System.out.println("Total neto: $"+totalNeto);
+        System.out.println("Total neto: $" + totalNeto);
     }
 
+    /**
+     * Calcula el costo del interes en base al aporte mensual.
+     *
+     * @param aporte aporte mensual.
+     * @param porcentajeInteres porcentaje de la tasa de interés.
+     * @return
+     */
     private Float calcularCostoInteres(Float aporte, Float porcentajeInteres) {
         return aporte * (porcentajeInteres / 100);
     }
 
+    /**
+     * Calcula el costo total del mes. Sumando el costo del interes en base al
+     * aporte mensual más el porcentaje de la tasa de interés.
+     *
+     * @param aporte aporte mensual.
+     * @param costoInteres costo del interes en base al aporte mensual.
+     * @return
+     */
     private Float calcularTotalMes(Float aporte, Float costoInteres) {
         return costoInteres + aporte;
     }
--- CalculadoraAhorro/src/Main.java
+++ CalculadoraAhorro/src/Main.java
@@ -1,25 +1,77 @@
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Float porcentajeInteres = -1f;
+        Integer meses = -1;
+        Float aporte = -1f;
+
         Scanner tec = new Scanner(System.in);
         System.out.println("-------- Calculadora de ahorros --------");
-        System.out.println("Porcentaje de la tasa de interés mensual:  ");
-        Float porcentajeInteres = tec.nextFloat();
-        System.out.println("Cantidad de meses que tendrá el dinero ahorrado:  ");
-        Integer meses = tec.nextInt();
+
+        //Un ciclo, mientras porcentaje de interés sea -1, se va a seguir preguntando el porcentaje 
+        //hasta que se ingrese una cifra válida, mayor de 0 y que sea Float
+        do {
+            try {
+                System.out.println("Porcentaje de la tasa de interés mensual:  ");
+                porcentajeInteres = tec.nextFloat();
+                if (porcentajeInteres < 0) {
+                    System.out.println("El porcentaje de interés no puede menor que 0");
+                    porcentajeInteres = -1f;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Favor de ingrese una cifra válida");
+                tec.next();
+            }
+        } while (porcentajeInteres < 0);
+
+        //Un ciclo, mientras el mes sea -1, se va a seguir pidiendo la cantidad 
+        //de meses hasta que se ingrese una cifra válida, mayor de 0 y que sea Integer
+        do {
+            try {
+
+                System.out.println("Cantidad de meses que tendrá el dinero ahorrado:  ");
+                meses = tec.nextInt();
+                if (meses < 0) {
+                    System.out.println("No puede ingresar meses menores a 0");
+                    meses = -1;
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Favor de ingrese un mes válido");
+                tec.next();
+
+            }
+        } while (meses < 0);
 
         ArrayList<Float> aportes = new ArrayList();
-        
+
+        //For que repite el ciclo hasta que se acaben los meses
         for (Integer i = 1; i <= meses; i++) {
-            System.out.println("Aporte mes " + i + ":");
-            Float aporte = tec.nextFloat();
+            //Un ciclo, mientras el aporte sea -1, se va a seguir pidiendo el aporte
+            //hasta que se ingrese una cifra válida, mayor de 0 y que sea Float
+            do {
+                try {
+
+                    System.out.println("Aporte mes " + i + ":");
+                    aporte = tec.nextFloat();
+                    if (aporte < 0) {
+                        System.out.println("No puede ingresar aportes menores a 0");
+                        meses = null;
+                    }
+
+                } catch (InputMismatchException e) {
+                    System.out.println("Favor de ingrese una cifra válida");
+                    tec.next();
+                }
+            } while (aporte < 0);
             aportes.add(aporte);
+            
         }
 
         Calculadora calculadora = new Calculadora();
